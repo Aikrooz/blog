ef2f4a2dab1aaa5a4578a45582c2a02b4736c8f4
aikrooz <aikrooz>
2025-12-18 02:43:46 +0100

    latest update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrin.auth import User
+from from django.utils import timezone
+STATUS[
+    ("Draft","Draft"),
+    ("Published","Published")
+]
 
-# Create your models here.
+ class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)#title
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish') #A short form of the inherited title but space is replaced with hyphen(-) and unique with date
+    author = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    class Meta:
+        ordering = ('-publish',)
